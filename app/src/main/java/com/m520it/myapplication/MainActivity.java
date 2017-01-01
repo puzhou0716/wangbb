880c17662c5d9d09d1e0cb1390b5cb7a47e198e2
Administrator <dfddffff@163.com>
2017-01-01 23:08:14 +0800

    fsfsfsafsf

--- app/src/main/java/com/m520it/myapplication/MainActivity.java
+++ app/src/main/java/com/m520it/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		// 继续开发
+		// 继续开发 添加了好多东西
+		// 新的内容
     }
 }
